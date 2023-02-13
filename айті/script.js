1ca95410be06f0ba83b1305553a1bdac900e0045
inflMETEOR <125122045+inflMETEOR@users.noreply.github.com>
2023-02-13 21:07:34 +0200

    Add files via upload

---
+++ айті/script.js
@@ -0,0 +1,12 @@
+// function firstTask(){
+//     for(i=5; i<=10; i++){
+//         console.log(i)
+//     }
+// }
+// firstTask()
+
+function taskKalk(a, b){
+    console.log(a+b)
+}
+taskKalk(2, 3)
+taskKalk(10,2)
